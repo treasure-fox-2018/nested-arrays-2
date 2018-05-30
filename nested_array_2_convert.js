3785b0e478228a1caf39dbe3d56cd806f65c645c
rizariza69 <riza.riza69@gmail.com>
2018-05-30 22:22:58 +0700

    nested aray done

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -11,8 +11,18 @@ let roster = [
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
+function convert_roster_format(nestedArray) {
   // your convert code here
+  let arrHasil = [];
+
+for (let i = 1; i < nestedArray.length ; i++) {
+    let obj = {}
+    for (let j = 0; j < nestedArray[i].length; j++) {
+      obj[nestedArray[0][j]] = nestedArray[i][j];
+    }
+    arrHasil.push(obj)
+  }
+  return arrHasil
 }
 
 let object_roster = convert_roster_format(roster)
