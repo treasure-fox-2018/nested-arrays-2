c4bbe4e6aff8b1cf66f6c27148b1bc4ee8e9f183
dawimuhammad <muhamadhaddawi@gmail.com>
2018-05-30 16:00:24 +0700

    done - exercise nested_array_2_convert.js

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,15 +12,31 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+    let rosterObjKey = nestedArray[0];
+    let rosterArr = [];
+
+    for (let i = 1; i < nestedArray.length; i++) {
+        let playerObj = {};
+
+        for (let j = 0; j < rosterObjKey.length; j++) {
+            playerObj[rosterObjKey[j]] = nestedArray[i][j];
+        }
+
+        rosterArr.push(playerObj);
+    }
+
+    return rosterArr;
 }
 
 let object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+console.log(object_roster[2]);
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
 console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+
+
+console.log(object_roster);
 
 module.exports = {
   convert_roster_format
